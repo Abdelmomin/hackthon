557d9e0aa2ae89513d04d5722ec4e27de39d35e7
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-04-29 22:31:16 +0100

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/providers/AuthProvider";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +32,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
           {children}
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,33 @@
+"use client";
+
+import { UploadButton } from "@/utils/uploadthing";
+
 export default function Home() {
-  return <p>Home page</p>;
+  return (
+    <div>
+      <p>Home page</p>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            alert("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            alert(`ERROR! ${error.message}`);
+          }}
+          className="bg-slate-800 ut-label:text-lg ut-allowed-content:ut-uploading:text-red-300"
+          appearance={{
+            button:
+              "ut-ready:bg-green-500 ut-uploading:cursor-not-allowed rounded-r-none bg-red-500 bg-none after:bg-orange-400",
+            container: "w-max flex-row rounded-md border-cyan-300 bg-slate-800",
+            allowedContent:
+              "flex h-8 flex-col items-center justify-center px-2 text-white",
+          }}
+        />
+      </main>
+    </div>
+  );
 }
